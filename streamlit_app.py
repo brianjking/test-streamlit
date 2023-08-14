3119e21eb319227e8488d728c3112ca613c44834
Brian J King <brianjking@users.noreply.github.com>
2023-08-13 19:50:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,14 @@
+import os
+import smtplib
 import streamlit as st
-st.write('Hello World')
+from email.mime.text import MIMEText
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import WebBaseLoader
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+from datetime import datetime
+
+st.title('Personal Newsletter Summarization')
+st.sidebar.title('Admin & Actions')
+
